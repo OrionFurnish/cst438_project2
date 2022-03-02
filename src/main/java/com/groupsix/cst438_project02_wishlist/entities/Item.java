89d57591d31afdc4c55f75c90e967cec87e4aa1d
hernandez <alfhernandez@csumb.edu>
2022-03-02 13:35:57 -0800

    Database connection, edited entities, add queries to repository

---
+++ src/main/java/com/groupsix/cst438_project02_wishlist/entities/Item.java
@@ -0,0 +1,57 @@
+package com.groupsix.cst438_project02_wishlist.database;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer itemId;
+    private String itemUrl;
+    private String itemImgUrl;
+    private String itemName;
+    private String itemDetails;
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemUrl() {
+        return itemUrl;
+    }
+
+    public void setItemUrl(String itemUrl) {
+        this.itemUrl = itemUrl;
+    }
+
+    public String getItemImgUrl() {
+        return itemImgUrl;
+    }
+
+    public void setItemImgUrl(String itemImgUrl) {
+        this.itemImgUrl = itemImgUrl;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemDetails() {
+        return itemDetails;
+    }
+
+    public void setItemDetails(String itemDetails) {
+        this.itemDetails = itemDetails;
+    }
+}
---
+++ src/main/java/com/groupsix/cst438_project02_wishlist/entities/Wishlist.java
@@ -0,0 +1,57 @@
+package com.groupsix.cst438_project02_wishlist.database;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Map;
+
+@Entity
+public class Wishlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer wishlistId;
+    private Integer userId;
+    private String listName;
+
+//    // TODO: Create db table for wishlist attribute: itemsPriority
+//    //       key = itemId, value = priority. Used to store all our items in current list.
+//    //       potential issues.... items may have same priority. Maybe change this.
+//    // https://www.baeldung.com/java-jpa-lazy-collections
+//    @ElementCollection
+//    @MapKeyColumn(name = "itemId")
+//    private Map<Integer, Integer> items;
+
+    public Integer getWishlistId() {
+        return wishlistId;
+    }
+
+    public void setWishlistId(Integer wishlistId) {
+        this.wishlistId = wishlistId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getListName() {
+        return listName;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+
+//    public Map<Integer, Integer> getItems() {
+//        return items;
+//    }
+
+//    public void setItems(Map<Integer, Integer> items) {
+//        this.items = items;
+//    }
+}
