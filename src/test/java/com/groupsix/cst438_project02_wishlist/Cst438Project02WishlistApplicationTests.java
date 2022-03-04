704ee1b27c31e9b44f8458a11a2b0f91d5e88f06
hernandez <alfhernandez@csumb.edu>
2022-03-03 23:37:18 -0800

    Updated application.properties to use team database instead of personal

--- src/test/java/com/groupsix/cst438_project02_wishlist/Cst438Project02WishlistApplicationTests.java
+++ src/test/java/com/groupsix/cst438_project02_wishlist/Cst438Project02WishlistApplicationTests.java
@@ -1,7 +1,6 @@
 package com.groupsix.cst438_project02_wishlist;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
